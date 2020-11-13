45c6f6904a21d114124df8862e25d67fa3017547
quan <jiahao.quan@me.com>
2020-11-13 15:56:30 -0500

    environment setting

---
+++ src/main/java/com/coral/community/CoumminityApplication.java
@@ -0,0 +1,13 @@
+package com.coral.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoumminityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoumminityApplication.class, args);
+	}
+
+}
