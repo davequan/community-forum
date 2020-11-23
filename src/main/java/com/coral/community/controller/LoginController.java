04f8288918b22145b71c9a9442eedaa631de69e5
quan <jiahao.quan@me.com>
2020-11-23 00:41:13 -0500

    generate kaptcha

--- src/main/java/com/coral/community/controller/LoginController.java
+++ src/main/java/com/coral/community/controller/LoginController.java
@@ -3,19 +3,33 @@ package com.coral.community.controller;
 import com.coral.community.entity.User;
 import com.coral.community.service.UserService;
 import com.coral.community.util.CommunityConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+    private  static  final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer kaptchaproducer;
 
     @RequestMapping(path = "/register",method = RequestMethod.GET)
     public  String getRegisterPage() {
@@ -62,5 +76,22 @@ public class LoginController implements CommunityConstant {
         return "site/operate-result";
     }
 
+    @RequestMapping(path =  "/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response , HttpSession session) throws IOException {
+        // generate kaptcha
+        String text = kaptchaproducer.createText();
+        BufferedImage image = kaptchaproducer.createImage(text);
+        //save kaptch into session
+        session.setAttribute("kaptcha",text);
+        //send image to browser
+        response.setContentType("image/png");
+        try {
+            ServletOutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        } catch (IOException e) {
+            logger.error("kaptcha fail",e.getMessage());
+        }
+
+    }
 
 }
