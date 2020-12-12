2a36cb99ec9bd39f31fe74eed0c0f76d2e558da7
quan <jiahao.quan@me.com>
2020-12-12 10:19:55 -0500

    kafka demo

---
+++ src/test/java/com/coral/community/BlockingQueueTests.java
@@ -0,0 +1,53 @@
+package com.coral.community;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueTests {
+    public static void main(String[] args) {
+        BlockingQueue queue = new ArrayBlockingQueue(10);
+        new Thread(new Producer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+        new Thread(new Consumer(queue)).start();
+    }
+}
+class Producer implements  Runnable{
+    private BlockingQueue<Integer> queue;
+    public Producer(BlockingQueue<Integer> queue){
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try{
+            for(int i = 0; i< 100; i++){
+                Thread.sleep(20);
+                queue.put(i);
+                System.out.println(Thread.currentThread().getName()+"produce:"+queue.size());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+class Consumer implements  Runnable{
+    private BlockingQueue<Integer> queue;
+    public   Consumer (BlockingQueue<Integer> queue){
+        this.queue = queue;
+    }
+    @Override
+    public void run() {
+        try{
+            while(true) {
+                Thread.sleep(new Random().nextInt(1000));
+                queue.take();
+                System.out.println(Thread.currentThread().getName()+"consumer"+queue.size());
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
